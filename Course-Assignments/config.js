58288f60f4c7adc1e1968c89f1af790bb5db36a6
1nternetUser <43967720+1nternetUser@users.noreply.github.com>
2020-10-24 20:50:28 -0700

    Create config.js

---
+++ Course-Assignments/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "fir-hostingdemo-005307350",
+  storageBucket: "fir-hostingdemo-005307350.appspot.com",
+  messagingSenderId: "1006967984151",
+  appId: "1:1006967984151:web:dbfceb6fd675e8e8100c52",
+};
+
+firebase.initializeApp(config);
