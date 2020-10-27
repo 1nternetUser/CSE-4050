cc9e59d4ce8e5b05cfed7c548bb8ad3f6c27e745
1nternetUser <43967720+1nternetUser@users.noreply.github.com>
2020-10-26 18:48:49 -0700

    Add files via upload

---
+++ Web/public/config.js
@@ -0,0 +1,16 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+(function()
+{
+	const firebaseConfig = {
+		apiKey: "AIzaSyDOl_Y6xfLdpFOPUwJQ_Ans4Ed0ANvO9eY",
+		authDomain: "fir-hostingdemo-005307350.firebaseapp.com",
+		databaseURL: "https://fir-hostingdemo-005307350.firebaseio.com",
+		projectId: "fir-hostingdemo-005307350",
+		storageBucket: "fir-hostingdemo-005307350.appspot.com",
+		messagingSenderId: "1006967984151",
+		appId: "1:1006967984151:web:dbfceb6fd675e8e8100c52",
+		measurementId: "G-8Q1PD9HR7N"
+	};
+})()
+
+firebase.initializeApp(firebaseConfig);
