3d65e0c9559af3d6e90bffce40cf625b926c894e
1nternetUser <43967720+1nternetUser@users.noreply.github.com>
2020-11-12 18:54:46 -0800

    Create app.js

---
+++ Cloud-Firestore/app.js
@@ -0,0 +1,15 @@
+(function(){
+  var firebaseConfig = {
+    apiKey: "AIzaSyAElA8_oUBMUA-TVNHvWXBJ5Hut_qGTJWk",
+    authDomain: "cloudfirestoreassignment.firebaseapp.com",
+    databaseURL: "https://cloudfirestoreassignment.firebaseio.com",
+    projectId: "cloudfirestoreassignment",
+    storageBucket: "cloudfirestoreassignment.appspot.com",
+    messagingSenderId: "196075834727",
+    appId: "1:196075834727:web:aac1594b84b2a31f7833c2",
+    measurementId: "G-0NQ3PPZ59Z"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  //firebase.analytics();
+})();
